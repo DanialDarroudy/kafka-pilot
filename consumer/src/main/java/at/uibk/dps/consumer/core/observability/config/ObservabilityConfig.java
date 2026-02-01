6758d959228ed9d74351a94e02065c699f926ef7
DanialDarroudy <d.darroudi@mohaymen.ir>
2026-02-01 19:36:39 +0330

    add observability configuration for consumer.

---
+++ consumer/src/main/java/at/uibk/dps/consumer/core/observability/config/ObservabilityConfig.java
@@ -0,0 +1,31 @@
+package at.uibk.dps.consumer.core.observability.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "observability")
+public class ObservabilityConfig {
+    private Log log = new Log();
+    private Metric metric = new Metric();
+    private Otel otel = new Otel();
+
+    @Data
+    public static class Log {
+        private int exportTimeoutSeconds = 10;
+    }
+
+    @Data
+    public static class Metric {
+        private int readIntervalSeconds = 15;
+        private int exportTimeoutSeconds = 10;
+    }
+
+    @Data
+    public static class Otel {
+        private String collectorGrpcEndpoint;
+        private int connectionTimeoutSeconds= 5;
+    }
+}
---
+++ consumer/src/main/java/at/uibk/dps/consumer/core/observability/di/ObservabilityDependencyInstaller.java
@@ -0,0 +1,60 @@
+package at.uibk.dps.consumer.core.observability.di;
+
+import at.uibk.dps.consumer.core.observability.config.ObservabilityConfig;
+import io.opentelemetry.api.logs.Logger;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ObservabilityDependencyInstaller {
+    private final ObservabilityConfig config;
+
+    @Bean
+    public Meter getMeter() {
+        var exporter = OtlpGrpcMetricExporter.builder()
+                .setConnectTimeout(Duration.ofSeconds(config.getOtel().getConnectionTimeoutSeconds()))
+                .setTimeout(Duration.ofSeconds(config.getMetric().getExportTimeoutSeconds()))
+                .setEndpoint(config.getOtel().getCollectorGrpcEndpoint())
+                .build();
+
+        var reader = PeriodicMetricReader.builder(exporter)
+                .setInterval(Duration.ofSeconds(config.getMetric().getReadIntervalSeconds()))
+                .build();
+
+        var provider = SdkMeterProvider.builder()
+                .registerMetricReader(reader)
+                .build();
+
+        return provider.get("ConsumerMeter");
+    }
+
+    @Bean
+    public Logger getLogger() {
+        var exporter = OtlpGrpcLogRecordExporter.builder()
+                .setConnectTimeout(Duration.ofSeconds(config.getOtel().getConnectionTimeoutSeconds()))
+                .setTimeout(Duration.ofSeconds(config.getLog().getExportTimeoutSeconds()))
+                .setEndpoint(config.getOtel().getCollectorGrpcEndpoint())
+                .build();
+
+        var processor = BatchLogRecordProcessor.builder(exporter)
+                .build();
+
+        var provider = SdkLoggerProvider.builder()
+                .addLogRecordProcessor(processor)
+                .build();
+
+        return provider.get("ConsumerLogger");
+    }
+
+}
