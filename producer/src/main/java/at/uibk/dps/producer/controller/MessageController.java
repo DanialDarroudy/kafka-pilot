8ad3c4abc625cf3838caa5349c258bd6aed2f951
DanialDarroudy <d.darroudi@mohaymen.ir>
2026-02-22 20:53:29 +0330

    remove counter message in producer.

--- producer/src/main/java/at/uibk/dps/producer/controller/MessageController.java
+++ producer/src/main/java/at/uibk/dps/producer/controller/MessageController.java
@@ -2,8 +2,7 @@ package at.uibk.dps.producer.controller;
 
 import at.uibk.dps.producer.module.message.send.abstraction.IMessageSender;
 import at.uibk.dps.producer.module.message.send.dto.SendMessageRequestDto;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,20 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
 public class MessageController {
     private final IMessageSender messageSender;
-    private final LongCounter counter;
-
-    public MessageController(IMessageSender messageSender, Meter meter) {
-        this.messageSender = messageSender;
-        this.counter = meter.counterBuilder("producer.messages.received")
-                .setDescription("Messages received via REST API")
-                .build();
-    }
 
     @PostMapping()
     public ResponseEntity<Object> sendMessage(@RequestBody SendMessageRequestDto dto) {
-        counter.add(1);
         messageSender.send(dto);
         return ResponseEntity.ok().build();
     }
