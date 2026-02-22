0fa44d86c1e9a45cdc12ff790fdbdc8d2378a1f1
DanialDarroudy <d.darroudi@mohaymen.ir>
2026-02-22 20:59:20 +0330

    remove counter message in consumer.

--- consumer/src/main/java/at/uibk/dps/consumer/module/message/poll/business/MessagePoller.java
+++ consumer/src/main/java/at/uibk/dps/consumer/module/message/poll/business/MessagePoller.java
@@ -1,8 +1,8 @@
 package at.uibk.dps.consumer.module.message.poll.business;
 
+import at.uibk.dps.consumer.core.config.ConsumerConfig;
 import at.uibk.dps.consumer.module.message.poll.abstraction.IMessagePoller;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
@@ -11,17 +11,12 @@ import java.time.Duration;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
+@RequiredArgsConstructor
 public class MessagePoller implements IMessagePoller {
     private final AtomicBoolean running = new AtomicBoolean(false);
     private KafkaConsumer<String, byte[]> consumer;
     private Thread pollThread;
-    private final LongCounter counter;
-
-    public MessagePoller(Meter meter) {
-        this.counter = meter.counterBuilder("consumer.messages.polled")
-                .setDescription("Messages polled")
-                .build();
-    }
+    private ConsumerConfig config;
 
     @Override
     public synchronized void start(KafkaConsumer<String, byte[]> consumer) {
@@ -44,8 +39,7 @@ public class MessagePoller implements IMessagePoller {
     private void run() {
         try {
             while (running.get()) {
-                var records = consumer.poll(Duration.ofMillis(500));
-                counter.add(records.count());
+                var records = consumer.poll(Duration.ofMillis(2L * config.getFetchMaxWaitMs()));
                 consumer.commitAsync();
             }
         } catch (WakeupException ignored) {
